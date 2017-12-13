69d511cd436b508fc3a25e9ca216bdc4749c6bd6
Anthony Di Falco <adifalco@nd.edu>
2017-12-13 13:20:41 -0500

    Final update

--- web/static/js/faces.js
+++ web/static/js/faces.js
@@ -1,13 +1,13 @@
 $(window).bind('load', function(){
 
+	var last,diff;
 	var timer = parseInt($('#ProbeTime')[0].value);
 	$("#ProbeID").removeClass("hidden");
 	setTimeout(function(){
 		$("#Images").removeClass("hidden");
 		$("#ProbeID").addClass("hidden");
+		last = event.timeStamp;
 	}, timer);
-	var last,diff;
-	last = event.timeStamp;
 	$("#button1").click(function(event){
 		diff = parseInt(event.timeStamp-last);
 		$.cookie("timer",diff, {path: '/'});
